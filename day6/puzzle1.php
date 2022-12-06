14358cf6fae1d48618c2acb21bfb331ea3d76491
Gioia <gioialorusso@gmail.com>
2022-12-06 08:29:30 +0100

    Create puzzle1.php

---
+++ day6/puzzle1.php
@@ -0,0 +1,17 @@
+$input_file = 'marker.txt';
+$fh = fopen($input_file, 'r');
+$marker_length = 4;
+
+while (($line = fgets($fh)) !== FALSE) {
+    $marker = [];
+    for ($i = 0; $i < strlen($line); $i++){
+        for ($j = 0; $j < $marker_length; $j++){
+            $marker[] = $line[$i + $j];
+        }
+        if(count(array_unique($marker)) === $marker_length){
+            echo $i + $marker_length;
+            break;
+        }
+        $marker = [];
+    }
+}
